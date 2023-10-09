44b7e8e1e3a1b174b8ea93fe53f67dd38796818c
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-10-09 13:35:26 +0200

    fix tests, now passing (define Main class, use starter-test instead of test)

--- src/test/java/no/hvl/dat250/gruppe1/pollingproject/PollingProjectApplicationTests.java
+++ src/test/java/no/hvl/dat250/gruppe1/pollingproject/PollingProjectApplicationTests.java
@@ -2,8 +2,10 @@ package no.hvl.dat250.gruppe1.pollingproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes=SpringMain.class)
 class PollingProjectApplicationTests {
 
     @Test
